8ca6594e43d3a5cab521722188f39f6f0cf38ae1
hanhanchan <40458024+hanhanchan@users.noreply.github.com>
2023-06-26 14:03:28 +0800

    git add .

--- insp4c.sh
+++ insp4c.sh
@@ -1,8 +1,8 @@
 #p4c compiler
-git clone --recursive https://github.com/p4lang/p4c.git 
-sudo apt-get install -y cmake g++ git automake libtool libgc-dev bison flex libfl-dev libgmp-dev libboost-dev libboost-iostreams-dev libboost-graph-dev llvm pkg-config python python-scapy python-ipaddr python-ply tcpdump doxygen graphviz texlive-full
+#git clone --recursive https://github.com/p4lang/p4c.git 
+#sudo apt-get install -y cmake g++ git automake libtool libgc-dev bison flex libfl-dev libgmp-dev libboost-dev libboost-iostreams-dev libboost-graph-dev llvm pkg-config python python-scapy python-ipaddr python-ply tcpdump doxygen graphviz texlive-full
 #protobuf done before
-cd ~/p4c
+cd ~/csh/dependency/p4c
 mkdir build
 cd build
 cmake ..
