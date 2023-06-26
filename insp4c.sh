f0d7d9bf9bee0936438145dbcc0c8c65dcba2512
hanhanchan <40458024+hanhanchan@users.noreply.github.com>
2023-06-26 14:47:54 +0800

    git add .

--- insp4c.sh
+++ insp4c.sh
@@ -2,10 +2,10 @@
 #git clone --recursive https://github.com/p4lang/p4c.git 
 #sudo apt-get install -y cmake g++ git automake libtool libgc-dev bison flex libfl-dev libgmp-dev libboost-dev libboost-iostreams-dev libboost-graph-dev llvm pkg-config python python-scapy python-ipaddr python-ply tcpdump doxygen graphviz texlive-full
 #protobuf done before
-cd ~/csh/dependency/p4c
+# cd ~/csh/dependency/p4c
 #mkdir build
 cd build
-cmake ..
-# make -j4
-# make -j4 check
+# cmake ..
+make -j4
+make -j4 check
 # sudo make install
